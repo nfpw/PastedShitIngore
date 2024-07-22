565c8c7e10c285791ec851359702aec1f0056863
nfpw <153939922+nfpw@users.noreply.github.com>
2024-07-22 21:32:28 +0300

    Update GhostyDuckyy.lua

--- GhostyDuckyy.lua
+++ GhostyDuckyy.lua
@@ -1,17 +1,22 @@
 function obfuscate(source,VarName,WaterMark)
     warn("Started obfuscate")
-    local Variable = VarName or "Sex_"
+    local Variable = VarName or "Taurus_"
     local WM
+
     if source == nil then
         source = [[print("Hello World!")]]
     end
+
     local ticks = tick()
+
     if typeof(WaterMark) == "string" and WaterMark ~= nil then
-        WM = "    "..tostring(WaterMark).." | Secured by nfpw"
+        WM = "    "..tostring(WaterMark).." | Secure by nfpw"
     else
-        WM = "    ".."WaterMark".." | Secured by nfpw"
+        WM = "    ".."WaterMark".." | Secure by nfpw"
     end
+
     WM = "--[[".."\n".. tostring(WM) .."\n".."]]--".."\n\n"
+
     local random_ = function(length)
         local letters = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"}
         local code
@@ -21,6 +26,7 @@ function obfuscate(source,VarName,WaterMark)
         else
             code = ""..tostring(letters[math.random(1,26)]):upper()
         end
+
         for i = 1, tonumber(length) do
             if math.random(1,2) == 1 then -- letters
                 local get_letters = letters[math.random(1,26)]
@@ -35,8 +41,10 @@ function obfuscate(source,VarName,WaterMark)
         end
         return tostring(code)
     end
+
     local StringToBinary = function(String)
         local BinaryString = {}
+
         for i, Character in ipairs(String:split'') do
             local Binary = ""
             local Byte = Character:byte()
@@ -46,43 +54,49 @@ function obfuscate(source,VarName,WaterMark)
             end
             table.insert(BinaryString, string.format("%.8d", Binary))
         end
+
         return table.concat(BinaryString, " ")
     end
+
     local add_binary = function(number, s)
         local highest = 1
+
         local topic = {
             "Deobfuscate?",
             "HelloWorld!",
-            "IronBrewFork?Nope.",
-            "PSUFork?Nope.",
-            "TouchSomeGrass",
-            "NigaRizz",
+            "GhostyFork?Yes",
+            "Skidded?Yes",
+            "Touchsomegrass",
+            "imgonagetdumpedsohard",
             "nfpw",
-            "Pengu",
-            "NigerLuki",
+            "ObfuscatedRetard",
             "Sex",
-            "SigmaSkibiRizz",
-            "Minors",
             random_(math.random(50,150)),
         }
+
         for i,v in pairs(topic) do
             if i > highest then
                 highest = i
             end
         end
+
         for i = 1, tonumber(number) do
             local str = [[local ]]..Variable..tostring(random_(math.random(10,12)))..[[ = ]]..'"'..StringToBinary(tostring(topic[math.random(1, tonumber(highest))]))..'"'.."; "
             s = s..str
         end
+
             return tostring(s)
         end
+
     local Random_Variable = {
-        TableByte = random_(math.random(2,132)),
-        Table_concat = random_(math.random(2,132)),
-        Loadstring = random_(math.random(2,132)),
+        TableByte = random_(math.random(15,20)),
+        Table_concat = random_(math.random(15,20)),
+        Loadstring = random_(math.random(15,20)),
     }
-    local troll_func = [[function() ]]..add_binary(math.random(30,90), "")..[[ end]]
-    local troll_var = [[local ]]..Variable..tostring(random_(math.random(15,221)))..[[ = ]]..troll_func
+
+    local troll_func = [[function() ]]..add_binary(math.random(30,50), "")..[[ end]]
+    local troll_var = [[local ]]..Variable..tostring(random_(math.random(15,20)))..[[ = ]]..troll_func
+
     local SourceByte = ""
     for i = 1,string.len(source) do SourceByte = SourceByte..'"\\'..string.byte(source, i)..'", ' end
     local TableByte = [[local ]]..Variable..tostring(Random_Variable.TableByte)..[[ = {]]..SourceByte..[[}]]
@@ -91,30 +105,37 @@ function obfuscate(source,VarName,WaterMark)
         [1] = Variable..tostring(Random_Variable.Loadstring),
         [2] = Variable..tostring(Random_Variable.TableByte),
     }
+
     local fake_code = function(number,r)
         local t = {}
             for i = 1, tonumber(number) do
                 local create_Var = Variable..tostring(random_(math.random(15,20)))
                 local random
+
                 if r ~= nil then
                     random = "return "..tostring(random_(tonumber(r)))
                 else
                     random = "return "..tostring(random_(math.clamp(1000, string.len(source) / 2, string.len(source))))
                 end
+
                 local byte = ""
                 for x = 1,string.len(random) do byte = byte..'"\\'..string.byte(random, x)..'", ' end
                 local fake = [[local ]]..create_Var..[[ = {]]..byte..[[}; ]]..[[local ]]..create_Var.." = "..func[1]..[[(]]..create_Var..[[); ]]
                 table.insert(t,fake)
             end
+
             return unpack(t)
         end
+
     local obfuscated = WM..troll_var.."; "..Loadstring.."; "..fake_code(math.random(2,4), math.random(400,600))..TableByte.."; "..[[local ]]..Variable..tostring(random_(math.random(15,20))).." = "..func[1].."("..func[2]..")".."; "..fake_code(math.random(2,4), math.random(string.len(source) / 2, string.len(source) * 2))
     setclipboard(obfuscated)
     warn("Done obfuscate in "..tostring(tick() - ticks).." second")
 return
 end
 
+--// Module
 return function(source,CustomVarName,WaterMark)
 task.spawn(function()
     obfuscate(source,CustomVarName,WaterMark)
 end)
+end
