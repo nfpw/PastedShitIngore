d7884bcf2307a57d4bfc0f868620a6dcc6553060
nfpw <153939922+nfpw@users.noreply.github.com>
2024-07-24 18:58:24 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -1221,7 +1221,7 @@ function ArrayFieldLibrary:CreateWindow(Settings)
 	Main.Shadow.Image.ImageTransparency = 1
 	LoadingFrame.Version.TextTransparency = 1
 	local imageLabel = Instance.new("ImageLabel")
-    	imageLabel.Image = "http://www.roblox.com/asset/?id=18636144002"
+    	imageLabel.Image = "http://www.roblox.com/asset/?id=18636143937"
     	imageLabel.Size = UDim2.new(0, 300, 0, 200)
     	imageLabel.Position = UDim2.new(0.64, 0, 0.48, 0)
     	imageLabel.AnchorPoint = Vector2.new(0.5, 0.5)
